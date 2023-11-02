6d4e4979b6f709296104107269ce2d15ba53358e
Frostshake <vanillaphase@protonmail.com>
2023-11-02 19:36:40 +0000

    vmangos import script progress

--- contrib/vmangos_db_import/db_import.py
+++ contrib/vmangos_db_import/db_import.py
@@ -69,14 +69,14 @@ def remove_obsolete_worldstates():
     trinity_world_con.commit()
     
     
-remove_obsolete_area_triggers()
-remove_obsolete_battleground_templates()
-remove_obsolete_instances()
+# remove_obsolete_area_triggers()
+# remove_obsolete_battleground_templates()
+# remove_obsolete_instances()
 
-remove_obsolete_transports()    
-# #TODO Clean up waypoint_data, waypoint scripts
-remove_obsolete_world_safe_locs()
-remove_obsolete_worldstates()
+# remove_obsolete_transports()    
+# # #TODO Clean up waypoint_data, waypoint scripts
+# remove_obsolete_world_safe_locs()
+# remove_obsolete_worldstates()
     
 def gameobject_template_check_vmangos():
     has_more = True
@@ -138,15 +138,11 @@ def gameobject_template_check_vmangos():
                 
                 if match[0][3] != result[3]:
                     name_mismatch += 1
-                    print(match[0][3])
-                    print(result[3])
-                    print('----')
                 
         
         offset = offset + 500  
         has_more = len(results) > 0
 
-    
     print('missing')
     print(missing)
     
@@ -156,9 +152,141 @@ def gameobject_template_check_vmangos():
     print("name mismatch")
     print(name_mismatch)
 
+def gameobject_check_vmangos():
+    has_more = True
+    src_obj_query = ("SELECT guid, id, map, position_x, position_y, position_z FROM gameobject LIMIT 500 OFFSET %s") 
+    dest_obj_query = ("SELECT guid, id, map, position_x, position_y, position_z FROM gameobject "
+                    "WHERE id = %s AND map = %s AND patch_max = 10")
     
-gameobject_template_check_vmangos()
+    delete_src_obj_entity_queries = [
+        ("DELETE FROM gameobject_addon WHERE guid = %s"),
+        ("DELETE FROM game_event_gameobject WHERE guid = %s"),
+        ("DELETE FROM game_event_gameobject_quest WHERE id = %s"),
+        ("DELETE FROM gameobject WHERE guid = %s"),
+    ]
     
+    #TODO gameobject_loot_template
+    
+    missing = 0
+    offset = 0
+    while has_more:
+        tri_world_cur.execute(src_obj_query, (offset,))
+        results = tri_world_cur.fetchall()
+            
+        for result in results:
+            vm_world_cur.execute(dest_obj_query,(result[1],result[2]))
+            matches = vm_world_cur.fetchall()
+            
+            matches_len = len(matches)
+            
+            if(matches_len == 0):
+                for del_q in delete_src_obj_entity_queries:
+                        tri_world_cur.execute(del_q, (result[0],))
+                        
+                offset -= 1
+                missing += 1
+                trinity_world_con.commit()
+        
+        offset += 500
+        has_more = len(results) > 0
+        
+    print("missing")
+    print(missing)
+    
+def create_gameobject(vm_go_guid, tri_go_guid = None):
+    src_query = "SELECT * FROM gameobject WHERE guid = %s"
+    vm_world_cur.execute(src_query, (vm_go_guid,))
+    result = vm_world_cur.fetchone()
+    
+    dest_insert = (
+        "INSERT INTO gameobject ("
+        "id, map, zoneId, areaId, spawnDifficulties, phaseUseFlags, PhaseId, PhaseGroup, terrainSwapMap, "
+        "position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, "
+        "spawntimesecs, animprogress, state, ScriptName, VerifiedBuild"
+        ") VALUES ("
+        "%s, %s, 0, 0, 0, 0, 0, 0, -1, "
+        "%s, %s, %s, %s, %s, %s, %s, %s,"
+        "%s, %s, %s, \"\", 40618"
+        ")"
+        )
+    
+    dest_update = (
+        "UPDATE gameobject SET "
+        "position_x = %s, position_y = %s, position_z=%s, orientation=%s, rotation0=%s, rotation1=%s, rotation2=%s, rotation3=%s, "
+        "spawntimesecs = %s, animprogress=%s, state = %s, VerifiedBuild = 40618 "
+        "WHERE guid = %s"
+    )
+    
+    if(tri_go_guid == None):
+        tri_world_cur.execute(dest_insert, (
+            result[1], result[2], 
+            result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10],
+            result[11], result[13], result[14],
+        ))
+    else:
+         tri_world_cur.execute(dest_update, (
+            result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10],
+            result[11], result[13], result[14],
+           result[0] 
+        ))
+    
+    trinity_world_con.commit()
+    
+def gameobject_update_against_vmangos():
+    has_more = True
+    src_obj_query = ("SELECT guid, id, map, position_x, position_y, position_z FROM gameobject WHERE patch_max = 10 LIMIT 1000 OFFSET %s") 
+    dest_obj_query = ("SELECT guid, id, map, position_x, position_y, position_z FROM gameobject "
+                    "WHERE id = %s "
+                    "AND position_x BETWEEN %s AND %s "
+                    "AND position_y BETWEEN %s AND %s "
+                    "AND map = %s")
+    missing = 0
+    simple_match = 0
+    offset = 0
+    while has_more:
+        vm_world_cur.execute(src_obj_query, (offset,))
+        results = vm_world_cur.fetchall()
+            
+        for result in results:
+            #TODO currently this requires to be ran multiple times, changing diff each time
+            diff = 5
+            tri_world_cur.execute(dest_obj_query,(
+                                    result[1],
+                                    result[3] - diff,
+                                    result[3] + diff,
+                                    result[4] - diff,
+                                    result[4] + diff,
+                                    result[2],
+                                   ))
+            matches = tri_world_cur.fetchall()
+            
+            matches_len = len(matches)
+            if(matches_len == 0):
+                missing += 1
+                offset -= 1
+                create_gameobject(result[0])
+            elif(matches_len == 1):
+                simple_match += 1
+                create_gameobject(result[0], matches[0][0])
+            else:
+                continue
+                # TODO handle multiple cases in a single zone.
+         
+        
+        offset += 1000
+        print(offset)
+        has_more = len(results) > 0
+        
+    print("missing")
+    print(missing)
+    
+    print("simple match")
+    print(simple_match)
+    
+    
+# gameobject_template_check_vmangos()
+# gameobject_check_vmangos()
+# gameobject_update_against_vmangos()
     
 vm_world_cur.close()
 tri_world_cur.close()
