a82fbd357db60183273c7f56526dd9cb718d33d6
Frostshake <vanillaphase@protonmail.com>
2024-05-05 17:33:46 +0100

    pool import progress

--- contrib/vmangos_db_import/importer/pool.py
+++ contrib/vmangos_db_import/importer/pool.py
@@ -3,8 +3,6 @@
 import constants
 import database as db
 
-#TODO
-
 TRINITY_POOL_TYPE_CREATURE = 0
 TRINITY_POOL_TYPE_GAMEOJECT = 1
 TRINITY_POOL_TYPE_POOL = 2 
@@ -36,26 +34,61 @@ def handle_pool_templates():
 
     
 def handle_pool_members():
-    vm_member_pools = db.vm_world.get_rows_raw("SELECT pool_id, mother_pool, chance, description FROM pool_pool")
+    vm_member_pools = db.vm_world.select_all(
+        db.SelectQuery("pool_pool")
+    )
     
     for vm_mp in vm_member_pools:
-        _upsert_pool_member(TRINITY_POOL_TYPE_POOL, vm_mp[0], vm_mp[1], vm_mp[2], vm_mp[3])
-        
-        
-    #TODO handle creatures
-    #TODO handle game objects
+        _upsert_pool_member(TRINITY_POOL_TYPE_POOL, vm_mp['pool_id'], vm_mp['mother_pool'], vm_mp['chance'], vm_mp['description'])
+    
+    
+    #TODO handle creature template and GO template pools.
+    
+    vm_creature_pools = db.vm_world.select_chunked(
+        db.SelectQuery("pool_creature").order_by("guid ASC, pool_entry ASC"),
+        250
+    )
+    
+    for vm_cp in vm_creature_pools:
+        _upsert_pool_member(TRINITY_POOL_TYPE_CREATURE, vm_cp['guid'], vm_cp['pool_entry'], vm_cp['chance'], vm_cp['description'])
+    
+    vm_go_pools = db.vm_world.select_chunked(
+        db.SelectQuery("pool_gameobject").order_by("guid ASC, pool_entry ASC"),
+        250
+    )
+    
+    for vm_gp in vm_go_pools:
+        _upsert_pool_member(TRINITY_POOL_TYPE_GAMEOJECT, vm_gp['guid'], vm_gp['pool_entry'], vm_gp['chance'], vm_gp['description'])
+
     
             
-def _upsert_pool_member(type, spawn_id, pool_spawn_id, chance, desc):
-        existing = db.tri_world.get_row_raw(
-            "SELECT type FROM pool_members WHERE type = %s AND spawnId = %s AND poolSpawnId = %s", 
-            (type, spawn_id, pool_spawn_id,)
+def _upsert_pool_member(type, spawn_id, pool_spawn_id, chance, desc):   
+    existing = db.tri_world.get_row_raw(
+        "SELECT type FROM pool_members WHERE type = %s AND spawnId = %s", 
+        (type, spawn_id,)
+        )
+    
+    if existing == None:
+        db.tri_world.upsert(
+            db.UpsertQuery("pool_members").values({
+                'type': type,
+                'spawnId': spawn_id,
+                'poolSpawnId': pool_spawn_id,
+                'chance': chance,
+                'description': desc
+            })
+        )
+    else:
+        db.tri_world.upsert(
+            db.UpsertQuery("pool_members").values({
+                'chance': chance,
+                'description': desc,
+                'poolSpawnId': pool_spawn_id
+            }).where(
+                db.GroupCondition("AND")
+                    .condition("type", "=", type)
+                    .condition("spawnId", "=", spawn_id)
             )
-        
-        if existing == None:
-            db.tri_world.execute_raw("INSERT INTO pool_members (type, spawnId, poolSpawnId, chance, description )",
-                                 (type, spawn_id, pool_spawn_id, chance, desc,))
-        else:
-            db.tri_world.execute_raw("UPDATE pool_members SET chance = %s, description = %s WHERE type = %s AND spawnId = %s AND poolSpawnId = %s",
-                                 (chance, desc, type, spawn_id, pool_spawn_id,))
+        )
+
     
