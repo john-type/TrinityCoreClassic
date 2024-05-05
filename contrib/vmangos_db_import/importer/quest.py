3c70f0193b04dbcb1a7b0434fa0df2691b5bf662
Frostshake <vanillaphase@protonmail.com>
2024-05-05 16:32:21 +0100

    tidy quest import script

--- contrib/vmangos_db_import/importer/quest.py
+++ contrib/vmangos_db_import/importer/quest.py
@@ -7,7 +7,7 @@ import database as db
 
 def Import():
     import_templates_vmangos()
-    #remove_modern()
+    remove_modern()
     
 def remove_modern():
     db.tri_world.chunk_raw(
@@ -38,187 +38,201 @@ def _handle_obsolete_modern_row(row):
     return -1
     
 def import_templates_vmangos():
-    db.vm_world.chunk_raw(
-        ("SELECT entry, Method, QuestLevel, MinLevel, MaxLevel, "
-         "Type, SuggestedPlayers, "
-         "Title, Details, Objectives, EndText, "
-         "PrevQuestId, NextQuestId, ExclusiveGroup, BreadcrumbForQuestId, "
-         "SrcSpell, SrcItemId, SrcItemCount, "
-         "RequiredClasses, RequiredSkill, RequiredSkillValue, "
-         "RequiredMinRepFaction, RequiredMinRepValue, RequiredMaxRepFaction, RequiredMaxRepValue "
-         "FROM quest_template LIMIT %s OFFSET %s"),
-        500,
-        _handle_import_template_row
+    
+    rows = db.vm_world.select_chunked(
+        db.SelectQuery("quest_template").order_by("entry ASC"),
+        250
     )
+    
+    for row in rows:
+        existing_record = db.tri_world.select_one(
+                db.SelectQuery("quest_template").where("ID", "=", row['entry'])
+            )
         
-def _handle_import_template_row(row):
-    dest_template_query = "SELECT ID, LogTitle FROM quest_template WHERE ID = %s"
-    
-    match_row = db.tri_world.get_row_raw(dest_template_query, (row[0],))
-    
-    if(match_row == None):
-        _upsert_quest_template(row)
-    else: 
-        _upsert_quest_template(row, match_row)
-    
-    return 0
+        _upsert_quest_template(row, existing_record)
+
 
 def _upsert_quest_template(vm_qt, tri_qt = None):
-    if tri_qt == None:
-        return #TODO handle insert
-    
+
     #TODO quest_offer_reward
     #TODO rewards
     #TODO other tables.
     
-    #TODO more fields
-    update_query = ("UPDATE quest_template SET "
-    "QuestType = %s, QuestLevel = %s, MinLevel = %s, "
-    "QuestInfoID = %s, SuggestedGroupNum = %s, "
-    "LogTitle = %s, QuestDescription = %s, LogDescription = %s, AreaDescription = %s, "
-    "StartItem = %s, "
-    "VerifiedBuild = 40618, Expansion = 0 "
-    "WHERE ID = %s"
+    #TODO handle check all fields.
+    
+    #TODO check how flags can be translated.
+    flags = vm_qt['QuestFlags']
+    flags_ex = 0
+    flags_ex2 = 0
+    
+    upsert_query = db.UpsertQuery("quest_template").values({
+        'QuestType': vm_qt['Method'],
+        'QuestLevel': vm_qt['QuestLevel'],
+        'MinLevel': vm_qt['MinLevel'],
+        'QuestSortID': vm_qt['ZoneOrSort'],
+        'QuestInfoID': vm_qt['Type'],
+        'SuggestedGroupNum': vm_qt['SuggestedPlayers'],
+        'RewardNextQuest': vm_qt['NextQuestInChain'],
+        'RewardDisplaySpell1': vm_qt['RewSpell'],
+        'RewardSpell': vm_qt['RewSpellCast'],
+        'StartItem': vm_qt['SrcItemId'],
+        'Flags': flags,
+        'FlagsEx': flags_ex,
+        'FlagsEx2': flags_ex2,
+        'RewardItem1': vm_qt['RewItemId1'],
+        'RewardAmount1': vm_qt['RewItemCount1'],
+        'RewardItem2': vm_qt['RewItemId2'],
+        'RewardAmount2': vm_qt['RewItemCount2'],
+        'RewardItem3': vm_qt['RewItemId3'],
+        'RewardAmount3': vm_qt['RewItemCount3'],
+        'RewardItem4': vm_qt['RewItemId4'],
+        'RewardAmount4': vm_qt['RewItemCount4'],
+        'ItemDrop1': vm_qt['ReqSourceId1'],
+        'ItemDropQuantity1': vm_qt['ReqSourceCount1'],
+        'ItemDrop2': vm_qt['ReqSourceId2'],
+        'ItemDropQuantity2': vm_qt['ReqSourceCount2'],
+        'ItemDrop3': vm_qt['ReqSourceId3'],
+        'ItemDropQuantity3': vm_qt['ReqSourceCount3'],
+        'ItemDrop4': vm_qt['ReqSourceId4'],
+        'ItemDropQuantity4': vm_qt['ReqSourceCount4'],
+        'RewardChoiceItemID1': vm_qt['RewChoiceItemId1'],
+        'RewardChoiceItemQuantity1': vm_qt['RewChoiceItemCount1'],
+        'RewardChoiceItemID2': vm_qt['RewChoiceItemId2'],
+        'RewardChoiceItemQuantity2': vm_qt['RewChoiceItemCount2'],
+        'RewardChoiceItemID3': vm_qt['RewChoiceItemId3'],
+        'RewardChoiceItemQuantity3': vm_qt['RewChoiceItemCount3'],
+        'RewardChoiceItemID4': vm_qt['RewChoiceItemId4'],
+        'RewardChoiceItemQuantity4': vm_qt['RewChoiceItemCount4'],
+        'RewardChoiceItemID5': vm_qt['RewChoiceItemId5'],
+        'RewardChoiceItemQuantity5': vm_qt['RewChoiceItemCount5'],
+        'RewardChoiceItemID6': vm_qt['RewChoiceItemId6'],
+        'RewardChoiceItemQuantity6': vm_qt['RewChoiceItemCount6'],
+        'POIContinent': vm_qt['PointMapId'],
+        'POIx': vm_qt['PointX'],
+        'POIy': vm_qt['PointY'],
+        'POIPriority': vm_qt['PointOpt'],
+        'RewardFactionID1': vm_qt['RewRepFaction1'],
+        'RewardFactionValue1': vm_qt['RewRepValue1'],
+        'RewardFactionID2': vm_qt['RewRepFaction2'],
+        'RewardFactionValue2': vm_qt['RewRepValue2'],
+        'RewardFactionID3': vm_qt['RewRepFaction3'],
+        'RewardFactionValue3': vm_qt['RewRepValue3'],
+        'RewardFactionID4': vm_qt['RewRepFaction4'],
+        'RewardFactionValue4': vm_qt['RewRepValue4'],
+        'RewardFactionID5': vm_qt['RewRepFaction5'],
+        'RewardFactionValue5': vm_qt['RewRepValue5'],
+        'Expansion': 0,
+        'LogTitle': vm_qt['Title'],
+        'LogDescription': vm_qt['Objectives'],
+        'QuestDescription': vm_qt['Details'],
+        'AreaDescription': vm_qt['EndText'],
+        'VerifiedBuild': constants.TargetBuild,
+    })
+    
+    if tri_qt == None:
+        upsert_query.values({
+            "ID": vm_qt['entry']
+        })
+    else:
+        upsert_query.where("ID", "=", tri_qt['ID'])
+        
+    db.tri_world.upsert(upsert_query)
+            
+    existing_addon = db.tri_world.select_one(
+        db.SelectQuery("quest_template_addon").where("ID", "=", vm_qt['entry'])
     )
     
-    db.tri_world.execute_raw(update_query, (
-        vm_qt[1], vm_qt[2], vm_qt[3], 
-        vm_qt[5], vm_qt[6],
-        vm_qt[7], vm_qt[8], vm_qt[9], vm_qt[10],
-        vm_qt[16],
-        vm_qt[0] #entry
-    ,))
-    
-    
-    existing_addon_entry = db.tri_world.get_row_raw("SELECT ID FROM quest_template_addon WHERE ID = %s", (vm_qt[0],))
-    
-    #TODO more fieldss
-    if existing_addon_entry == None:
-        # insert_query = (
-            
-        # )
-        #TODO handle insert
-        return
-    else:
-        update_query = (
-            "UPDATE quest_template_addon SET "
-            "MaxLevel = %s, "
-            "PrevQuestId = %s, NextQuestId = %s, ExclusiveGroup = %s, BreadcrumbForQuestId = %s, "
-            "SourceSpellID = %s, ProvidedItemCount = %s, "
-            "AllowableClasses = %s, RequiredSkillID = %s, RequiredSkillPoints =%s, "
-            "RequiredMinRepFaction = %s, RequiredMinRepValue = %s, RequiredMaxRepFaction = %s, RequiredMaxRepValue = %s "
-            "WHERE ID = %s"
-        )
-        
-        #TODO decide how to correctly handle column differences
-        next_quest_id = vm_qt[12]
-        if(next_quest_id < 0):
+    #TODO decide how to correctly handle column differences
+    next_quest_id = vm_qt['NextQuestId']
+    if(next_quest_id < 0):
             next_quest_id = 0
-        
-        db.tri_world.execute_raw(update_query, (
-            vm_qt[4],
-            vm_qt[11], next_quest_id, vm_qt[13], vm_qt[14],
-            vm_qt[15], vm_qt[17],
-            vm_qt[18], vm_qt[19],  vm_qt[20],
-            vm_qt[21], vm_qt[22], vm_qt[23], vm_qt[24],     
-            vm_qt[0] #entry
-        ))
-        
-    
-    _update_quest_objectives(vm_qt[0])
-    _update_quest_rewards(vm_qt[0])
-    
-    
-def _update_quest_objectives(quest_id):
-    vm_row = db.vm_world.get_row_raw((
-        "SELECT "
-        "ReqItemId1, ReqItemId2, ReqItemId3, ReqItemId4, " # objective item
-        "ReqItemCount1, ReqItemCount2, ReqItemCount3, ReqItemCount4, "
-        "ReqSourceId1, ReqSourceId2, ReqSourceId3, ReqSourceId4, "      #provided item
-        "ReqSourceCount1, ReqSourceCount2, ReqSourceCount3, ReqSourceCount4, "
-        "ReqCreatureOrGOId1, ReqCreatureOrGOId2, ReqCreatureOrGOId3, ReqCreatureOrGOId4, "#objective creature / GO
-        "ReqCreatureOrGOCount1, ReqCreatureOrGOCount2, ReqCreatureOrGOCount3, ReqCreatureOrGOCount4, " 
-        "ReqSpellCast1, ReqSpellCast2, ReqSpellCast3, ReqSpellCast4, "  #objectives spell
-        "RepObjectiveFaction, RepObjectiveValue, "  # min rep
-        "RewOrReqMoney, "
-        "ObjectiveText1, ObjectiveText2, ObjectiveText3, ObjectiveText4 "
-        "FROM quest_template WHERE entry = %s"
-        ), (quest_id,))
-    
 
+    #TODO translate special flags
+    special_flags = 0
+    addon_upsert_query = db.UpsertQuery("quest_template_addon").values({
+        'MaxLevel': vm_qt['MaxLevel'],
+        'AllowableClasses': vm_qt['RequiredClasses'],
+        'SourceSpellID': vm_qt['SrcSpell'],
+        'PrevQuestID': vm_qt['PrevQuestId'],
+        'NextQuestID': next_quest_id,
+        'ExclusiveGroup': vm_qt['ExclusiveGroup'],
+        'BreadcrumbForQuestId': vm_qt['BreadcrumbForQuestId'],
+        'RequiredSkillID': vm_qt['RequiredSkill'],
+        'RequiredSkillPoints': vm_qt['RequiredSkillValue'],
+        'RequiredMinRepFaction': vm_qt['RequiredMinRepFaction'],
+        'RequiredMaxRepFaction': vm_qt['RequiredMaxRepFaction'],
+        'RequiredMinRepValue': vm_qt['RequiredMinRepValue'],
+        'RequiredMaxRepValue': vm_qt['RequiredMaxRepValue'],
+        'ProvidedItemCount': vm_qt['SrcItemCount'],
+        'SpecialFlags': special_flags
+    })
     
-    if vm_row == None:
-        return
+    if existing_addon == None:
+        addon_upsert_query.values({
+            'ID': vm_qt['entry']
+        })
+    else:
+        addon_upsert_query.where("ID", "=", vm_qt['entry'])
+        
+    db.tri_world.upsert(addon_upsert_query)
+        
+    _update_quest_objectives(vm_qt['entry'], vm_qt)
     
-    #TODO handle ReqSource    
+    
+def _update_quest_objectives(quest_id, vm_qt):
+    
+    existing_objectives = db.tri_world.select_all(
+        db.SelectQuery("quest_objectives").where('QuestID', '=', quest_id).order_by("`Order` ASC")
+    )
+    
+    #TODO check all fields
     #TODO handle spellCast (trinity doesnt seem to use this, but the GO for it instead), possibly easier to manually add.
     #TODO ensure all objective types are covered.
-    
-    existing_objectives = db.tri_world.get_rows_raw("SELECT ID from quest_objectives WHERE QuestID = %s ORDER BY `Order` ASC", (quest_id,))
     
     #order is GO/Creature, Item, MinRep, Money
     #storage index appears to the same as order.
     #type for and above (money/currency and rep) appear to be -1 storage index.
     
-    intermediate_rows = []  #type, id, amount
+    intermediate_rows = [] #type, id, amount
     
     descriptions = [
-        vm_row[31], vm_row[32], vm_row[33], vm_row[34]
+        vm_qt['ObjectiveText1'],
+        vm_qt['ObjectiveText2'],
+        vm_qt['ObjectiveText3'],
+        vm_qt['ObjectiveText4']
     ]
     
     #creatures / GO
-    if vm_row[20] > 0:
-        if vm_row[16] > 0:
-            intermediate_rows.append([0, vm_row[16], vm_row[20]])
-        else:
-            intermediate_rows.append([2, vm_row[16], vm_row[20]])
-    
-    if vm_row[21] > 0:
-        if vm_row[17] > 0:
-            intermediate_rows.append([0, vm_row[17], vm_row[21]])
-        else:
-            intermediate_rows.append([2, vm_row[17], vm_row[21]])
-            
-    if vm_row[22] > 0:
-        if vm_row[18] > 0:
-            intermediate_rows.append([0, vm_row[18], vm_row[22]])
-        else:
-            intermediate_rows.append([2, vm_row[18], vm_row[22]])
-            
-    if vm_row[23] > 0:
-        if vm_row[19] > 0:
-            intermediate_rows.append([0, vm_row[19], vm_row[23]])
-        else:
-            intermediate_rows.append([2, vm_row[19], vm_row[23]])
+    for i in range(1, 4 + 1):
+        count_name = 'ReqCreatureOrGOCount' + str(i)
+        id_name = 'ReqCreatureOrGOId' + str(i)
+        if vm_qt[count_name] > 0:
+            row_type = 0 if vm_qt[id_name] > 0 else 2
+            intermediate_rows.append([row_type, vm_qt[id_name], vm_qt[count_name]])
     
     #items
-    if vm_row[0] > 0 and vm_row[4] > 0:
-        intermediate_rows.append([1, vm_row[0], vm_row[4]])
-        
-    if vm_row[1] > 0 and vm_row[5] > 0:
-        intermediate_rows.append([1, vm_row[1], vm_row[5]])
-        
-    if vm_row[2] > 0 and vm_row[6] > 0:
-        intermediate_rows.append([1, vm_row[2], vm_row[6]])
-        
-    if vm_row[3] > 0 and vm_row[7] > 0:
-        intermediate_rows.append([1, vm_row[3], vm_row[7]])
-        
-    #rep
-    if vm_row[28] > 0:
-        intermediate_rows.append([6, vm_row[28], vm_row[29]])
+    for i in range(1, 4 + 1):
+        count_name = 'ReqItemCount' + str(i)
+        id_name = 'ReqItemId' + str(i)
+        if vm_qt[count_name] > 0:
+            intermediate_rows.append([1, vm_qt[id_name], vm_qt[count_name]])
     
+    #rep
+    if vm_qt['RepObjectiveValue'] > 0:
+        intermediate_rows.append([6, vm_qt['RepObjectiveFaction'], vm_qt['RepObjectiveValue']])
+            
     #money
-    if vm_row[30] < 0:
-        intermediate_rows.append([8, 0, vm_row[30]])
-        
+    if vm_qt['RewOrReqMoney'] < 0:
+        intermediate_rows.append([8, 0, vm_qt['RewOrReqMoney']])
+            
+            
     order_index = 0
     storage_index = 0
     for intermediate in intermediate_rows:
         existing_id = 0
         
         if len(existing_objectives) > order_index:
-            existing_id = existing_objectives[order_index][0]
+            existing_id = existing_objectives[order_index]['ID']
             
             
         effective_storage_index = -1
@@ -231,84 +245,33 @@ def _update_quest_objectives(quest_id):
         
         #TODO confirm descriptions match objectives. 
         #TODO handle flags.
-    
+        objective_upsert = db.UpsertQuery("quest_objectives").values({
+            'QuestID': quest_id,
+            'type': intermediate[0],
+            '`Order`': order_index,
+            'StorageIndex': effective_storage_index,
+            'ObjectID': intermediate[1],
+            'Amount': intermediate[2],
+            'Flags': 0,
+            'Flags2': 0,
+            'ProgressBarWeight': 0,
+            'Description': description,
+            'VerifiedBuild': constants.TargetBuild
+        })
+        
         if existing_id > 0:
-            db.tri_world.execute_raw((
-                "UPDATE quest_objectives SET "
-                "`Type` = %s, `Order` = %s, StorageIndex = %s, "
-                "ObjectID = %s, Amount = %s, "
-                "Flags = 0, Flags2 = 0, ProgressBarWeight = 0, "
-                "Description = %s, VerifiedBuild = 40618 "
-                "WHERE ID = %s AND QuestID = %s"
-            ), (
-                intermediate[0], order_index, effective_storage_index,
-                intermediate[1], intermediate[2],
-                description,    
-                existing_id, quest_id,
-            ))
+            objective_upsert.where('ID', '=', existing_id)
         else:
-            next_id = db.tri_world.get_row_raw("SELECT MAX(ID) FROM quest_objectives")
-            next_id = next_id[0] + 1
+            last_id = db.tri_world.get_row_raw("SELECT MAX(ID) FROM quest_objectives")
+            objective_upsert.values({
+                'ID': last_id[0] + 1
+            })
             
-            db.tri_world.execute_raw((
-                "INSERT INTO quest_objectives ("
-                "ID, QuestId, `Type`, `Order`, StorageIndex, "
-                "ObjectID, Amount, "
-                "Flags, Flags2, ProgressBarWeight, Description, VerifiedBuild"
-                ") VALUES ("
-                "%s, %s, %s, %s, %s, "
-                "%s, %s, "
-                "0, 0, 0, %s, 40618"
-                ")"
-            ), (
-                next_id, quest_id, intermediate[0], order_index, effective_storage_index,
-                intermediate[1], intermediate[2],
-                description,
-            ))
-            
+        db.tri_world.upsert(objective_upsert)
+        
         order_index += 1
+        
+        #TODO delete obsolete objectives.
+        
     
-    #TODO delete obsolete objectives.
-    
-    
-def _update_quest_rewards(quest_id):
-    vm_row = db.vm_world.get_row_raw((
-        "SELECT "
-        "RewChoiceItemId1, RewChoiceItemId2, RewChoiceItemId3, RewChoiceItemId4, RewChoiceItemId5, RewChoiceItemId6,"
-        "RewChoiceItemCount1, RewChoiceItemCount2, RewChoiceItemCount3, RewChoiceItemCount4, RewChoiceItemCount5, RewChoiceItemCount6, "
-        "RewItemId1, RewItemId2, RewItemId3, RewItemId4, "
-        "RewItemCount1, RewItemCount2, RewItemCount3, RewItemCount4, "
-        "RewRepFaction1, RewRepFaction2, RewRepFaction3, RewRepFaction4, RewRepFaction5, "
-        "RewRepValue1, RewRepValue2, RewRepValue3, RewRepValue4, RewRepValue5, "
-        "RewXP, RewOrReqMoney, RewMoneyMaxLevel, "  #TODO implement in update
-        "RewSpell, RewSpellCast, RewMailTemplateId, RewMailDelaySecs, RewMailMoney " #TODO implement in update
-        "FROM quest_template WHERE entry = %s"
-    ), (quest_id,))
-    
-    if vm_row == None:
-        return
-    
-    update_query = (
-     "UPDATE quest_template SET "
-     "RewardChoiceItemID1 = %s, RewardChoiceItemID2 = %s, RewardChoiceItemID3 = %s, "
-     "RewardChoiceItemID4 = %s, RewardChoiceItemID5 = %s, RewardChoiceItemID6 = %s, "
-     "RewardChoiceItemQuantity1 = %s, RewardChoiceItemQuantity2 = %s, RewardChoiceItemQuantity3 = %s, "
-     "RewardChoiceItemQuantity4 = %s, RewardChoiceItemQuantity5 = %s, RewardChoiceItemQuantity6 = %s, "
-     "RewardItem1 = %s, RewardItem2 = %s, RewardItem3 = %s, RewardItem4 = %s, "
-     "RewardAmount1 = %s, RewardAmount2 = %s, RewardAmount3 = %s, RewardAmount4 = %s, "
-     "RewardFactionID1 = %s, RewardFactionID2 = %s, RewardFactionID3 = %s,RewardFactionID4 = %s, RewardFactionID5 = %s,"
-     "RewardFactionValue1 = %s, RewardFactionValue2 = %s, RewardFactionValue3 = %s,RewardFactionValue4 = %s, RewardFactionValue5 = %s "
-     "WHERE ID = %s"   
-    )
-    
-    db.tri_world.execute_raw(update_query, (
-            vm_row[0], vm_row[1], vm_row[2], #choice
-            vm_row[3], vm_row[4], vm_row[5], #choice
-            vm_row[6], vm_row[7], vm_row[8], #choice
-            vm_row[9], vm_row[10], vm_row[11], #choice
-            vm_row[12], vm_row[13], vm_row[14], vm_row[15], #item
-            vm_row[16], vm_row[17], vm_row[18], vm_row[19], #item
-            vm_row[20], vm_row[21], vm_row[22], vm_row[23], vm_row[24], #faction
-            vm_row[25], vm_row[26], vm_row[27], vm_row[28], vm_row[29], #faction        
-            quest_id
-        ))
+ 
