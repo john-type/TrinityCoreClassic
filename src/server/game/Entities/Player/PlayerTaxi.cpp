acc5d66d329d365af6db5672f087568e2fe18bdb
Frostshake <vanillaphase@protonmail.com>
2024-10-23 13:20:30 +0100

    Fixed incorrect taxi nodes assigned at char creation.

--- src/server/game/Entities/Player/PlayerTaxi.cpp
+++ src/server/game/Entities/Player/PlayerTaxi.cpp
@@ -40,34 +40,17 @@ void PlayerTaxi::InitTaxiNodesForLevel(uint32 race, uint32 chrClass, uint8 level
     // race specific initial known nodes: capital and taxi hub masks
     switch (race)
     {
-        case RACE_HUMAN:
-        case RACE_DWARF:
-        case RACE_NIGHTELF:
-        case RACE_GNOME:
-        case RACE_DRAENEI:
-        case RACE_WORGEN:
-        case RACE_PANDAREN_ALLIANCE:
-            SetTaximaskNode(2);     // Stormwind, Elwynn
-            SetTaximaskNode(6);     // Ironforge, Dun Morogh
-            SetTaximaskNode(26);    // Lor'danel, Darkshore
-            SetTaximaskNode(27);    // Rut'theran Village, Teldrassil
-            SetTaximaskNode(49);    // Moonglade (Alliance)
-            SetTaximaskNode(94);    // The Exodar
-            break;
-        case RACE_ORC:
-        case RACE_UNDEAD_PLAYER:
-        case RACE_TAUREN:
-        case RACE_TROLL:
-        case RACE_BLOODELF:
-        case RACE_GOBLIN:
-        case RACE_PANDAREN_HORDE:
-            SetTaximaskNode(11);    // Undercity, Tirisfal
-            SetTaximaskNode(22);    // Thunder Bluff, Mulgore
-            SetTaximaskNode(23);    // Orgrimmar, Durotar
-            SetTaximaskNode(69);    // Moonglade (Horde)
-            SetTaximaskNode(82);    // Silvermoon City
-            SetTaximaskNode(384);   // The Bulwark, Tirisfal
-            break;
+    case RACE_HUMAN:    SetTaximaskNode(2);  break;     // Human
+    case RACE_ORC:      SetTaximaskNode(23); break;     // Orc
+    case RACE_DWARF:    SetTaximaskNode(6);  break;     // Dwarf
+    case RACE_NIGHTELF: SetTaximaskNode(26);
+        SetTaximaskNode(27); break;     // Night Elf
+    case RACE_UNDEAD_PLAYER: SetTaximaskNode(11); break;// Undead
+    case RACE_TAUREN:   SetTaximaskNode(22); break;     // Tauren
+    case RACE_GNOME:    SetTaximaskNode(6);  break;     // Gnome
+    case RACE_TROLL:    SetTaximaskNode(23); break;     // Troll
+    case RACE_BLOODELF: SetTaximaskNode(82); break;     // Blood Elf
+    case RACE_DRAENEI:  SetTaximaskNode(94); break;     // Draenei
     }
 
     // new continent starting masks (It will be accessible only at new map)
